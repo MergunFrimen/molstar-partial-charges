6c14ff919533909d2dc3cb4ca89f4894f168e485
Dominik Tichy <tichydominik451@gmail.com>
2023-01-19 02:17:05 +0100

    vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'build',
+    reportCompressedSize: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",
+        chunkFileNames: "[name].js",
+        entryFileNames: "[name].js",
+      }
+    }
+  }
 })
