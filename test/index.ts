0f912388fc506a1009b05951bf364d8235630683
Dominik Tichy <tichydominik451@gmail.com>
2023-01-24 00:27:28 +0100

    feat: added example use of plugin

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,12 @@
 import './style.css'
-import ACC2Wrapper from '../src/index'
+import PartialChargesWrapper from '../src/index'
 
-var url_prefix = 'http://127.0.0.1:1338/examples/acc2/';
-var url = url_prefix + '3bj1.default2.cif';
+// Example use of the plugin wrapper
 
-const molstar = new ACC2Wrapper();
+const url_prefix = 'http://localhost:1338/examples/acc2/';
+const url = url_prefix + '3bj1.default2.cif';
+
+const molstar = new PartialChargesWrapper();
 
 molstar.init('app').then(() => {
     molstar.load(url);
@@ -16,40 +18,39 @@ addControl('3bj1.default.cif', async () => await molstar.load(url_prefix + '3bj1
 addControl('3bj1.default2.cif', async () => await molstar.load(url_prefix + '3bj1.default2.cif'))
 addControl('default-example.cif', async () => await molstar.load(url_prefix + 'default-example.cif'))
 addControl('7qo1.cif', async () => await molstar.load(url_prefix + '7qo1.cif'))
+addControl('6qga.cif', async () => await molstar.load(url_prefix + '6qga.charges.cif'))
 
 addHeader('Type');
 addControl('Cartoon', () => molstar.type.cartoon())
 addControl('Surface', () => molstar.type.surface())
 addControl('Ball and stick', () => molstar.type.ballAndStick())
+addControl('Empty', () => molstar.type.set(undefined, undefined))
             
 addSeparator();
 addHeader('Coloring');
 addControl('Default', () => molstar.coloring.default())
-addControl('Partial Charges (true, -1, 1)', () => molstar.coloring.partialCharges({ absolute: true, min: -1, max: 1}))
-addControl('Partial Charges (true, -0.2, 0.4)', () => molstar.coloring.partialCharges({ absolute: true, min: -0.2, max: 0.4}))
-addControl('Partial Charges (true, -0.38171, 0.38171)', () => molstar.coloring.partialCharges({ absolute: true, min: -0.38171, max: 0.38171}))
+addControl('Partial Charges (true, -1, 1)', () => molstar.coloring.partialCharges({ absolute: true, max: 1}))
+addControl('Partial Charges (true, -0.2, 0.4)', () => molstar.coloring.partialCharges({ absolute: true, max: 0.4}))
+addControl('Partial Charges (true, -0.38171, 0.38171)', () => molstar.coloring.partialCharges({ absolute: true, max: 0.38171}))
 addControl('Partial Charges (false)', () => molstar.coloring.partialCharges({ absolute: false }))
 
 addSeparator();
 addHeader('Charge range');
-addInput('min-charge', -5);
 addInput('max-charge', 5);
-addInput('absolute-toggle', false);
 addControl('Apply range', () => molstar.coloring.partialCharges({
-    absolute: eval((document.getElementById('absolute-toggle') as HTMLInputElement).value),
-    min: parseFloat((document.getElementById('min-charge') as HTMLInputElement)!.value),
+    absolute: true,
     max: parseFloat((document.getElementById('max-charge') as HTMLInputElement).value),
 }));
 
 addSeparator();
 addHeader('Change TypeId');
 addInput('type-id', 1);
-addControl('Set typeId', () => molstar.charges.setTypeId(
+addControl('Set typeId', () => molstar.charges.set(
     parseInt((document.getElementById('type-id') as HTMLInputElement).value),
 ))
 
 function addInput(id, placeholder) {
-    var input = document.createElement('input');
+    const input = document.createElement('input');
     input.setAttribute('id', id);
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', placeholder);
@@ -57,19 +58,19 @@ function addInput(id, placeholder) {
 }
 
 function addControl(label, action) {
-    var btn = document.createElement('button');
+    const btn = document.createElement('button');
     btn.onclick = action;
     btn.innerText = label;
     document.getElementById('controls')!.appendChild(btn);
 }
 
 function addSeparator() {
-    var hr = document.createElement('hr');
+    const hr = document.createElement('hr');
     document.getElementById('controls')!.appendChild(hr);
 }
 
 function addHeader(header) {
-    var h = document.createElement('h3');
+    const h = document.createElement('h3');
     h.innerText = header;
     document.getElementById('controls')!.appendChild(h);
 }
