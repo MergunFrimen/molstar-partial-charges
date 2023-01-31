26059e7978fe82a37ec780a53a6c2dae7f45327d
Dominik Tichy <tichydominik451@gmail.com>
2023-01-31 15:21:03 +0100

    chore: renamed test file

--- examples/main.ts
+++ examples/main.ts
@@ -1,5 +1,5 @@
-import './style.css'
-import ACC2PartialChargesWrapper from '../src/index'
+import './style.css';
+import ACC2PartialChargesWrapper from '../src/main';
 
 /**
  * Example use of the plugin wrapper
@@ -8,9 +8,8 @@ import ACC2PartialChargesWrapper from '../src/index'
 // file hosting the examples
 const url_prefix = 'http://localhost:1338/examples/output/';
 const url = url_prefix + '146d.cif.charges.cif';
-
 let current_example = 0;
-const examples  = [
+const examples = [
     '100d.cif.charges.cif',
     '101m.cif.charges.cif',
     '146d.cif.charges.cif',
@@ -36,104 +35,108 @@ const examples  = [
 const molstar = new ACC2PartialChargesWrapper();
 
 // for debugging purposes
-(window as any).molstar = molstar;
+declare global {
+    interface Window {
+        molstar: typeof molstar;
+    }
+}
+window.molstar = molstar;
 
-const specs = {
+const pluginSpecs = {
     layout: {
         initial: {
             isExpanded: false,
-            showControls: true,
-        }
+            showControls: false,
+        },
     },
-    behaviors: []
+    behaviors: [],
 };
 
 (async () => {
-    await molstar.init('app', specs);
+    await molstar.init('app', pluginSpecs);
     await molstar.load(url);
-    
+
     await molstar.color.partialCharges();
-    if (molstar.type.isDefaultApplicable())
-        await molstar.type.default();
-    else
-        await molstar.type.ballAndStick();
-})();
+    if (molstar.type.isDefaultApplicable()) await molstar.type.default();
+    else await molstar.type.ballAndStick();
+})().then(() => ({}), console.error);
 
 addHeader('Load');
 addControl('Next example', nextExample);
 for (const example of examples) {
     addControl(example.replace('.charges.cif', ''), async () => {
         await molstar.load(url_prefix + example);
-        
+
         await molstar.color.partialCharges();
-        if (molstar.type.isDefaultApplicable())
-            await molstar.type.default();
-        else
-            await molstar.type.ballAndStick();
+        if (molstar.type.isDefaultApplicable()) await molstar.type.default();
+        else await molstar.type.ballAndStick();
     });
 }
 
 addHeader('Type');
-addControl('Cartoon', async () => await molstar.type.default())
-addControl('Surface', async () => await molstar.type.surface())
-addControl('Ball and stick', async () => await molstar.type.ballAndStick())
-            
+addControl('Cartoon', async () => await molstar.type.default());
+addControl('Surface', async () => await molstar.type.surface());
+addControl('Ball and stick', async () => await molstar.type.ballAndStick());
+
 addSeparator();
 addHeader('Coloring');
-addControl('Default', async () => await molstar.color.default())
+addControl('Default', async () => await molstar.color.default());
 addControl('Relative', async () => await molstar.charges.relative());
-addInput('max-charge', 0.2);
+addInput('max-charge', '0.2');
 addControl('Absolute', async () => {
     const value = (document.getElementById('max-charge') as HTMLInputElement).value;
-    let parsed;
-    if (value)
-        parsed = parseFloat(value);
-    else
-        parsed = 0.2;
+    const parsed = value ? parseFloat(value) : 0.2;
     await molstar.charges.absolute(parsed);
 });
 
 addSeparator();
 addHeader('Change type');
-addInput('type-id', 1);
-addControl('Set typeId', () => molstar.charges.setTypeId(
-    parseInt((document.getElementById('type-id') as HTMLInputElement).value),
-))
+addInput('type-id', '1');
+addControl('Set typeId', () =>
+    molstar.charges.setTypeId(parseInt((document.getElementById('') as HTMLInputElement).value))
+);
+addControl('Set typeId', async () => {
+    const value = (document.getElementById('type-id') as HTMLInputElement).value;
+    const parsed = value ? parseInt(value) : 1;
+    await molstar.charges.setTypeId(parsed);
+});
 
 async function nextExample() {
     console.clear();
     current_example = (current_example + 1) % examples.length;
     await molstar.load(url_prefix + `${examples[current_example % examples.length]}`);
     await molstar.color.partialCharges();
-    if (molstar.type.isDefaultApplicable())
-        await molstar.type.default();
-    else
-        await molstar.type.ballAndStick();
-(document.querySelector('#app > div > div > div:nth-child(1) > div.msp-layout-region.msp-layout-left > div > div > div.msp-left-panel-controls-buttons > button:nth-child(2)') as HTMLButtonElement)!.click();
+    if (molstar.type.isDefaultApplicable()) await molstar.type.default();
+    else await molstar.type.ballAndStick();
+    (
+        document.querySelector(
+            '#app > div > div > div:nth-child(1) > div.msp-layout-region.msp-layout-left > div > div > div.msp-left-panel-controls-buttons > button:nth-child(2)'
+        ) as HTMLButtonElement
+    )?.click();
 }
 
-function addInput(id: string, placeholder) {
+function addInput(id: string, placeholder: string) {
     const input = document.createElement('input');
     input.setAttribute('id', id);
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', placeholder);
-    document.getElementById('controls')!.appendChild(input);
+    document.getElementById('controls')?.appendChild(input);
 }
 
-function addControl(label, action) {
+function addControl(label: string, action: ((this: GlobalEventHandlers, ev: MouseEvent) => unknown) | null) {
     const btn = document.createElement('button');
     btn.onclick = action;
     btn.innerText = label;
-    document.getElementById('controls')!.appendChild(btn);
+    document.getElementById('controls')?.appendChild(btn);
 }
 
 function addSeparator() {
     const hr = document.createElement('hr');
-    document.getElementById('controls')!.appendChild(hr);
+    document.getElementById('controls')?.appendChild(hr);
 }
 
-function addHeader(header) {
+function addHeader(header: string) {
     const h = document.createElement('h3');
     h.innerText = header;
-    document.getElementById('controls')!.appendChild(h);
+    document.getElementById('controls')?.appendChild(h);
 }
