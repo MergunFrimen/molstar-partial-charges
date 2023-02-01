7edd6fee3a6e93759d33db1d18fa75b34a3a9e51
Dominik Tichy <tichydominik451@gmail.com>
2023-02-01 01:56:33 +0100

    fix(vite.config): updated for optimal prod build

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,18 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     define: {
         'process.env.NODE_ENV': '"production"',
     },
     build: {
         outDir: 'dist',
-        sourcemap: true,
-        minify: true,
-        manifest: true,
         reportCompressedSize: true,
         emptyOutDir: true,
         lib: {
-            name: 'Molstar Partial Charges',
-            entry: resolve(__dirname, 'src/main.ts'),
-            formats: ['es', 'cjs'],
+            name: 'MolstarPartialCharges',
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['umd', 'es'],
             fileName: (format) => `main.${format}.js`,
         },
     },
