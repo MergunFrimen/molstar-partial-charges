f38f38d73d8deb0cf8f3c7213d6301f603031617
Dominik Tichy <tichydominik451@gmail.com>
2023-02-08 02:36:52 +0100

    chore: vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,19 @@ export default defineConfig({
         outDir: 'dist',
         reportCompressedSize: true,
         emptyOutDir: true,
+        target: 'es2015',
         lib: {
             name: 'MolstarPartialCharges',
             entry: resolve(__dirname, 'src/index.ts'),
             formats: ['umd', 'es'],
             fileName: (format) => `index.${format}.js`,
+        },
+        modulePreload: {
+            polyfill: false,
+        },
+        watch: {
+            exclude: ['node_modules/**', 'dist/**'],
+            buildDelay: 1000,
         },
     },
     plugins: [dts()],
