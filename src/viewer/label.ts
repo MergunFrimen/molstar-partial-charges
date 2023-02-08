adf5dfe6958efddbc31ce30e2087a5e82adac595
Dominik Tichy <tichydominik451@gmail.com>
2023-02-08 15:33:29 +0100

    fix: decimal places for labels

--- src/viewer/label.ts
+++ src/viewer/label.ts
@@ -1,7 +1,6 @@
 import { OrderedSet } from 'molstar/lib/mol-data/int';
 import { Loci } from 'molstar/lib/mol-model/loci';
 import { StructureElement } from 'molstar/lib/mol-model/structure';
-import { LociLabel } from 'molstar/lib/mol-plugin-state/manager/loci-label';
 import { PluginBehavior } from 'molstar/lib/mol-plugin/behavior';
 import { PluginContext } from 'molstar/lib/mol-plugin/context';
 import { ACC2PropertyProvider } from './property';
@@ -34,9 +33,9 @@ export const ACC2LociLabelProvider = PluginBehavior.create({
                 const label =
                     this.ctx.managers.interactivity.props.granularity === 'residue' ? 'Residue charge' : 'Atom charge';
 
-                return `<strong>${label}: ${charge?.toFixed(3) || 'undefined'}</strong>`;
+                return `<strong>${label}: ${charge?.toFixed(4) || 'undefined'}</strong>`;
             },
-            group: (label: LociLabel): string => label.toString().replace(/Model [0-9]+/g, 'Models'),
+            group: (label: string): string => label.toString().replace(/Model [0-9]+/g, 'Models'),
             priority: 0,
         };
         register() {
